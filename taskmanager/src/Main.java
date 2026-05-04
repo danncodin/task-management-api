f759941cf8b019eb5f6367a8e65b522f7ebc5186
danncodin <danielmarto.unit@gmail.com>
2026-05-04 14:10:41 -0300

    Creating the project, applying POO in Tasks.

---
+++ taskmanager/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Tarefa t1 = new Tarefa(
+                1,
+                "Estudar Java",
+                "O basico sobre classes e POO",
+                "Pendente"
+        );
+        t1.exibir();
+        // Exibir uma resposta
+    }
+}
