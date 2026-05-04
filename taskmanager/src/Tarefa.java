7e2d8054bd744c4d7a87db588616261e569ec291
danncodin <danielmarto.unit@gmail.com>
2026-05-04 15:04:36 -0300

    feat: classe Tarefa com construtor, getters, setters e exibir

--- taskmanager/src/Tarefa.java
+++ taskmanager/src/Tarefa.java
@@ -1,15 +1,41 @@
 public class Tarefa {
-    int id;
-    String titulo;
-    String descricao;
-    String status;
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String status;
 
-    private Tarefa (int id, String titulo, String descricao, String status) {
+    public Tarefa(int id, String titulo, String descricao, String status) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.id = id;
         this.status = status;
     }
+    public int getID() {
+        return id;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public String getStatus(){
+        return status;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+    public void setStatus(String status){
+        this.status = status;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
     public void exibir () {
         System.out.println("ID : " + id + " | " + " Título " + titulo);
         System.out.println("Status: " + status);
